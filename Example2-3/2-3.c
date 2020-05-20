3a3c852bfc46bf69a8c60c81b4b71d2c1f88531f
성수 박 <whiteibescu@gmail.com>
2020-05-20 22:10:20 +0900

    제어 문자를 사용한 출력

--- Example2-3/2-3.c
+++ Example2-3/2-3.c
@@ -2,13 +2,14 @@
 
 int main(void)
 {
-	printf("Be happy\n");
-	printf("1234567890123467890\n");
+	printf("Be happy\n");					// "Be Happy"를 출력하고 줄을 바꿈(\n)
+	printf("1234567890123467890\n");		// 화면에 열 번호 출력하고 줄을 바꿈(\n)
 	printf("My\tfriend\n");
-	
-	printf("Goot\bd\tchance\n");
-
+	// "My"를 출력하고 탭 위치로 이동(\t) 후에 "friend"를 출력하고 줄을 바꿈(\n)
+	printf("Goot\bd\tchance\n");		// "Goot"를 출력하고 한 칸 왼쪽으로 이동(\b)
+	// t를 d로 바꾸고 탭 위치로 이동(\t) 후에 "chance"를 출력하고 줄을 바꿈(\n)
 	printf("Cow\rW\a\n");
+	// 맨 앞으로 이동(\r)해 C를 W를 바꾸고 벨소리(\a)를 내고 줄을 바꿈(\n)
 
 	return 0;
 } 
