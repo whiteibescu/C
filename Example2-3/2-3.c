211abcb48c2fe911352ed1443c0a92e605b046da
성수 박 <whiteibescu@gmail.com>
2020-05-20 21:26:21 +0900

    제어 문자를 사용한 출력

---
+++ Example2-3/2-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Be happy\n");
+	printf("1234567890123467890\n");
+	printf("My\tfriend\n");
+	
+	printf("Goot\bd\tchance\n");
+
+	printf("Cow\rW\a\n");
+
+	return 0;
+}
