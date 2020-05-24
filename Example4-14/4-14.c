65821991ddbb79e97ce4ecc4dcf1f694a5b94763
성수 박 <whiteibescu@gmail.com>
2020-05-24 21:29:53 +0900

    연산자 순위와 연산 방향

--- Example4-14/4-14.c
+++ Example4-14/4-14.c
@@ -7,11 +7,11 @@ int main(void)
 
 	res = a / b * 2;			//우선 순위가 같으므로 왼쪽으로부터 차례로 연산
 	printf("res = %d\n", res);
-	res = ++a * 3;
+	res = ++a * 3;				//a의 값을 1 증가시키가고 3을 곱한다.
 	printf("res = %d\n", res);
-	res = a > b && a != 5;
+	res = a > b && a != 5;		// a > b의 결과와 a != 5의 결과를 && 연산
 	printf("res = %d\n", res);
-	res = a % 3 == 0;
+	res = a % 3 == 0;			// a % 3의 값이 0과 같은지 확인
 	printf("res = %d\n", res);
 
 	return 0;
