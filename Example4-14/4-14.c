ee9ce059f8c568d7904f954d1a39ba3924701f3c
성수 박 <whiteibescu@gmail.com>
2020-05-24 21:28:30 +0900

    연산자 우선순위와 연산 방향

--- Example4-14/4-14.c
+++ Example4-14/4-14.c
@@ -5,6 +5,14 @@ int main(void)
 	int a = 10, b = 5;
 	int res;
 
-	res = a / b * 2;
+	res = a / b * 2;			//우선 순위가 같으므로 왼쪽으로부터 차례로 연산
 	printf("res = %d\n", res);
+	res = ++a * 3;
+	printf("res = %d\n", res);
+	res = a > b && a != 5;
+	printf("res = %d\n", res);
+	res = a % 3 == 0;
+	printf("res = %d\n", res);
+
+	return 0;
 }
