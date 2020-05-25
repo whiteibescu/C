5119925bcd230c3ef014fab5ff748e49873cb563
성수 박 <whiteibescu@gmail.com>
2020-05-25 20:35:49 +0900

    switch ~ case문의 사용

--- Example5-6/5-6.c
+++ Example5-6/5-6.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main(void)
+int main(void) 
 {
 	int rank = 2, m = 0;
 
-	switch (rank)		// rank의 값이 얼마인지 확인
+	switch (rank)		// rank의 값이 얼마인지 확인~
 	{
 	case 1:				// rank가 1이면
 		m = 300;		// m = 300을 수행하고
