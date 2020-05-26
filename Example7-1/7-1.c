7b18f0f5d054a33c05e6fe44dd75e5317705d275
성수 박 <whiteibescu@gmail.com>
2020-05-27 00:08:09 +0900

    주석완료

--- Example7-1/7-1.c
+++ Example7-1/7-1.c
@@ -5,19 +5,19 @@ int sum(int x, int y);						// sum 함수 선언 : 186쪽에서 설명합니다.
 int main(void)								// main 함수 시작
 {
 	int a = 10, b = 20;
-	int result;
+	int result;								// 두 정수를 더한 결과(result)를 저장할 변수
 
-	result = sum(a, b);
+	result = sum(a, b);						// sum 함수 호출
 	printf("result : %d\n", result);
 
 	return 0;
-}
+}											// main 함수의 끝
 
-int sum(int x, int y)
+int sum(int x, int y)						// sum 함수 정의 시작
 {
-	int temp;
+	int temp;								// 두 정수의 합을 잠시 저장할 변수
 
-	temp = x + y;
+	temp = x + y;							// x와 y의 합을 temp에 보관
 
-	return temp;
+	return temp;							// sum 함수의 끝
 }
