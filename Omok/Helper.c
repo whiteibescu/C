182c1134185ef8028a3b57320cfefd394128ef75
성수 박 <whiteibescu@gmail.com>
2020-06-05 13:34:21 +0900

    오목

---
+++ Omok/Helper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+#include <windows.h>
+#define PAN_SIZE 19
+
+// 분석 - 작년 프로젝트 소스들...
+int team01(int pan[][PAN_SIZE]), int* px, int* py)
+{
+	// 게임이 시작되었다.
+	// 나는 흑돌이다.
+	// 나는 백돌이다.
+	// 상대가 돌을 어디에 두었는가.
+	// 나는 어디에 돌을 들 것인가
+	return 0;
+}
