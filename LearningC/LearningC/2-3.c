57883c67879d1e1192c78c02d896214073eb7889
SungSoo Park <whiteibescu@gmail.com>
2020-11-14 21:57:50 +0900

    Reviewing C Language

---
+++ LearningC/LearningC/2-3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("Be happy\n");
+	printf("12345678901234567890");
+	printf("My\tfriend\n");
+	printf("Goot\bd\tchange\n");
+	printf("Cow\rW\a\n");
+
+	return 0;
+}
