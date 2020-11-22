18f006acbe2b934abea3f4a033df541c4b639aba
SungSoo Park <whiteibescu@gmail.com>
2020-11-22 17:12:50 +0900

    9-8

---
+++ LearningC/LearningC/9-8.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(void);
+
+int main()
+{
+	int a = 10, b = 20;
+
+	swap();
+	printf("a:%d, b:%d\n", a, b);
+
+
+	return 0;
+}
+
+void swap(void)
+{
+	int temp;
+
+	temp = a;
+	a = b;
+	a = temp;
+}
