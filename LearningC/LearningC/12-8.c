5c6ff4213abb7773d32d0533fe1da2b08b82c10f
SungSoo Park <whiteibescu@gmail.com>
2020-11-23 19:35:48 +0900

    12

---
+++ LearningC/LearningC/12-8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	char str1[80] = "strawberry";
+	char str2[80] = "apple";
+	char* ps1 = "banana";
+	char* ps2 = str2;
+
+	printf("최초 문자열 : %s\n", str1);
+	strcpy(str1, str2);
+	printf("바뀐 문자열 : %s\n", str1);
+}
